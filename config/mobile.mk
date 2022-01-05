2d454a6bd8abaa8622b0030a192171cf5be89e71
TH779 <i@779.moe>
2022-01-06 02:14:29 +0800

    kscope: Checkout apns-conf.xml from LineageOS
     * From LineageOS/android_vendor_lineage@2007edd.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I3d02676399454e4f7116fb627e8cc994e206cd6f

--- config/mobile.mk
+++ config/mobile.mk
@@ -7,6 +7,10 @@
 # Inherit common Kaleidoscope stuff
 $(call inherit-product, vendor/kscope/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/kscope/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
