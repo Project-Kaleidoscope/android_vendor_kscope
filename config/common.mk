CyanogenMod/android_vendor_cm

Enable ADB authentication by default.
Change-Id: I942a2df97ef769549b6f76b7c25241380e8df4f1

config/common.mk
@@ -66,6 +66,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt

Project-Kaleidoscope/android_vendor_kscope

config/common.mk
@@ -60,6 +60,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
+# ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+
 # Set GMS client ID base
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \