de6685e2f11052b050853336dd716ed62494c1cc
alk3pInjection <5e147612@kscope.ink>
2022-05-29 13:25:05 +0800

    kscope: add x86_64 emulator target
    Change-Id: Ib1bce648af8b0fc2378f17f57949bed32d6599f7

---
+++ build/target/product/AndroidProducts.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 Project Kaleidoscope
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/kscope_sdk_phone_x86_64.mk
---
+++ build/target/product/kscope_sdk_phone_x86_64.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 Project Kaleidoscope
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdk_phone_x86_64 device.
+$(call inherit-product, build/target/product/sdk_phone_x86_64.mk)
+
+# Inherit some common Kaleidoscope stuff.
+$(call inherit-product, vendor/kscope/config/common.mk)
+
+# Enable mainline checking.
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/media/bootanimation.zip
+
+# Overrides
+PRODUCT_NAME := kscope_sdk_phone_x86_64
+PRODUCT_MODEL := Kaleidoscope SDK built for x86_64
+
+# Disable face unlock functionality.
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
