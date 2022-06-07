70791e542b0e8017cb9f4b6e8a987cbecc6ff518
Kujou Yuko <i@779.moe>
2022-06-07 18:50:58 +0800

    config: Disable data roaming by default
     * It should be manually enabled by the user, otherwise
       additional charges may be incurred without awareness.
    
    Change-Id: I63cb4a4bd613f2e15aeefb14904d346cc3b76683
    Signed-off-by: Kujou Yuko <i@779.moe>

--- config/mobile.mk
+++ config/mobile.mk
@@ -7,6 +7,10 @@
 # Inherit common Kaleidoscope stuff
 $(call inherit-product, vendor/kscope/config/common.mk)
 
+# Data roaming
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.android.dataroaming=false
+
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/kscope/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
