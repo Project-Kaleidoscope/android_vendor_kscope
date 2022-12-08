5c70d0de33535a20d2ff7f3a0cacd22eef859e2a
LibXZR <i@xzr.moe>
2022-12-08 18:28:10 +0800

    kscope: Build ParallelSpace from source
    Change-Id: I255de4784067dc1df89669f6da0bf4b7d3abef6a
    Signed-off-by: LibXZR <i@xzr.moe>

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGES += \
     Gboard \
     KscopeSetupWizard \
     KscopeThemesStub \
-    ParallelSpacePrebuilt \
+    ParallelSpace \
     Raindrop \
     SimpleDeviceConfig \
     ThemePicker
